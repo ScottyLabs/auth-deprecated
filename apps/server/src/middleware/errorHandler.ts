eaabd98f1438a6cc38280b18a56ec6e8d9e1506a
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-11-26 22:25:29 +0000

    chore(start): set up express server

---
+++ apps/server/src/middleware/errorHandler.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import { ValidateError } from "tsoa";
+
+// From https://tsoa-community.github.io/docs/error-handling.html
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+
+  if (err instanceof Error) {
+    console.error(`Error ${req.path}`, err);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", details: err.message });
+  }
+
+  next();
+};
---
+++ apps/server/src/server.ts
@@ -0,0 +1,52 @@
+// import fs from "node:fs";
+import http from "node:http";
+// import { YAML } from "bun";
+import cors, { type CorsOptions } from "cors";
+import type { ErrorRequestHandler } from "express";
+import express from "express";
+// import swaggerUi, { type JsonObject } from "swagger-ui-express";
+// import { RegisterRoutes } from "../build/routes";
+import env from "./env";
+import { errorHandler } from "./middleware/errorHandler";
+import { notFoundHandler } from "./middleware/notFoundHandler";
+
+const app = express();
+app.use(express.json({ limit: "8mb" }));
+
+// Define CORS options
+const corsOptions: CorsOptions = {
+  origin: env.ALLOWED_ORIGINS_REGEX?.split(",").map(
+    (origin) => new RegExp(origin),
+  ),
+};
+app.use(cors(corsOptions));
+
+// Create HTTP server with Express app attached
+const server = http.createServer(app);
+
+// Swagger
+// const file = fs.readFileSync("./build/swagger.yaml", "utf8");
+// const swaggerDocument = YAML.parse(file) as JsonObject;
+// app.use("/swagger", express.static("./node_modules/swagger-ui-dist"));
+// app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+// RegisterRoutes(app);
+app.get("/", (_req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+// Register error and not found handlers
+app.use(errorHandler as ErrorRequestHandler);
+app.use(notFoundHandler);
+
+// Start the server
+const port = env.SERVER_PORT;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+  process.exit();
+});
